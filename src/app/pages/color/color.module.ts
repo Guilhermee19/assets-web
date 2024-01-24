f7e5c0a65f8bfa7049c80c5f07581ce5e556b395
Guilhermee19 <guilherme@noclaftech.com>
2024-01-23 23:04:08 -0300

    feat: tela cor

--- src/app/pages/color/color.module.ts
+++ src/app/pages/color/color.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ColorRoutingModule } from './color-routing.module';
+import { ColorComponent } from './color.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ColorComponent
+  ],
   imports: [
     CommonModule,
     ColorRoutingModule

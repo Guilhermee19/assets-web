bb5399417073ee5222553d499d8c3c5dbc0cabd2
Guilhermee19 <guilherme@noclaftech.com>
2024-01-23 23:02:45 -0300

    feat: editor text

--- src/app/pages/text/text.component.ts
+++ src/app/pages/text/text.component.ts
@@ -13,7 +13,57 @@ export class TextComponent {
     input: [''],
   });
 
-  loginSubmitHandler() {
-    console.log();
+  ignore: string[] = [
+    'a',
+    'o',
+    'as',
+    'os',
+    'de',
+    'dos',
+    'das',
+    'do',
+    'da',
+    'e',
+    'ou',
+    'para',
+    'por',
+    'no',
+    'na',
+    'nos',
+    'nas',
+  ];
+
+  loginSubmitHandler(event: any) {
+    const action = event.submitter.innerText;
+
+    const input = (this.form.get('input')?.value || '')?.toLowerCase();
+
+    if (action === 'MAIÚSCULO') {
+      this.form.patchValue({ input: input?.toUpperCase() });
+    }
+
+    if (action === 'minusculo') {
+      this.form.patchValue({ input: input?.toLowerCase() });
+    }
+
+    if (action === 'Inverter') {
+      this.form.patchValue({ input: input?.split('').reverse().join('') });
+    }
+
+    if (action === 'Primeira Letra') {
+      const words = input?.split(' ');
+      const capitalizedWords = words?.map((word) => {
+        if (this.ignore.includes(word)) {
+          return word;
+        } else return word.charAt(0).toUpperCase() + word.slice(1);
+      });
+      this.form.patchValue({ input: capitalizedWords?.join(' ') });
+    }
+
+    if (action === 'Primeira palavra') {
+      this.form.patchValue({
+        input: input?.charAt(0).toUpperCase() + input?.slice(1),
+      });
+    }
   }
 }

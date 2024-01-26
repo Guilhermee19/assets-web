51aa8ef0f507149c0de147ec507f72f4dac9cbcc
Guilhermee19 <guilherme@noclaftech.com>
2024-01-25 22:42:30 -0300

    feat: custom qrcode

--- src/app/pages/image/image.module.ts
+++ src/app/pages/image/image.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { QRCodeModule } from 'angularx-qrcode';
 import { MatInputModule } from '@angular/material/input';
-
+import { MatButtonModule } from '@angular/material/button';
 import { ImageRoutingModule } from './image-routing.module';
 import { ImageComponent } from './image.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatSliderModule} from '@angular/material/slider';
+
 @NgModule({
   declarations: [ImageComponent],
   imports: [
@@ -15,6 +17,8 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     FormsModule,
     ReactiveFormsModule,
     MatInputModule,
+    MatButtonModule,
+    MatSliderModule
   ],
 })
 export class ImageModule {}

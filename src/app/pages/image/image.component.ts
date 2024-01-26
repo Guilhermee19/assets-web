d1b16d4e48b333bac6c5f581b694fbb962aa166f
Guilhermee19 <guilherme@noclaftech.com>
2024-01-26 11:43:07 -0300

    feat: melhoria do visual do qrcode

--- src/app/pages/image/image.component.ts
+++ src/app/pages/image/image.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
-import { QRCodeElementType } from 'angularx-qrcode';
 @Component({
   selector: 'app-image',
   templateUrl: './image.component.html',
@@ -17,38 +16,38 @@ export class ImageComponent {
   });
 
   saveAsImage(parent: any) {
-    let parentElement = null
+    let parentElement = null;
 
-    parentElement = parent.qrcElement.nativeElement.querySelector("img").src
+    parentElement = parent.qrcElement.nativeElement.querySelector('img').src;
 
     if (parentElement) {
       // converts base 64 encoded image to blobData
-      const blobData = this.convertBase64ToBlob(parentElement)
+      const blobData = this.convertBase64ToBlob(parentElement);
       // saves as image
-      const blob = new Blob([blobData], { type: "image/png" })
-      const url = window.URL.createObjectURL(blob)
-      const link = document.createElement("a")
-      link.href = url
+      const blob = new Blob([blobData], { type: 'image/png' });
+      const url = window.URL.createObjectURL(blob);
+      const link = document.createElement('a');
+      link.href = url;
       // name of the file
-      link.download = "angularx-qrcode"
-      link.click()
+      link.download = 'angularx-qrcode';
+      link.click();
     }
   }
 
   private convertBase64ToBlob(Base64Image: string) {
     // split into two parts
-    const parts = Base64Image.split(";base64,")
+    const parts = Base64Image.split(';base64,');
     // hold the content type
-    const imageType = parts[0].split(":")[1]
+    const imageType = parts[0].split(':')[1];
     // decode base64 string
-    const decodedData = window.atob(parts[1])
+    const decodedData = window.atob(parts[1]);
     // create unit8array of size same as row data length
-    const uInt8Array = new Uint8Array(decodedData.length)
+    const uInt8Array = new Uint8Array(decodedData.length);
     // insert all character code into uint8array
     for (let i = 0; i < decodedData.length; ++i) {
-      uInt8Array[i] = decodedData.charCodeAt(i)
+      uInt8Array[i] = decodedData.charCodeAt(i);
     }
     // return blob image after conversion
-    return new Blob([uInt8Array], { type: imageType })
+    return new Blob([uInt8Array], { type: imageType });
   }
 }

c6587fd4cbbf32360009d956c3354276caee73c9
Kauã Landi <58404392+kaualandi@users.noreply.github.com>
2024-04-04 14:49:20 -0300

    git c "refactor: today count to async pipe"

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,19 +1,14 @@
-import { Component,  OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
-  today: Date = new Date;
-
-  ngOnInit(){
-    this.atualizarRelogio();
-    setInterval(() => this.atualizarRelogio(), 1000);
-  }
-
-  atualizarRelogio() {
-    this.today = new Date();
-  }
+export class HomeComponent {
+  now = interval(1000).pipe(
+    map(() => {
+      return new Date();
+    })
+  );
 }
